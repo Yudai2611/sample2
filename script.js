cc62698f33b76ff5b297c412e03d6be9735cb7a2
Yudai2611 <yudai-ogiuchi@kccs.co.jp>
2025-04-02 11:53:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hello, World! This script is running!");
+const https = require('https');
+
+const slackUrl = "https://hooks.slack.com/services/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";
+const message = {
+    text: "Hello, World! This script is running!"
+};
+
+const data = JSON.stringify(message);
+
+const options = {
+    hostname: 'hooks.slack.com',
+    path: '/services/T0833P36XN1/B0854H09E0H/lotvEMaBxFBFm1JV0vJ44ZGc',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+};
+
+const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    res.setEncoding('utf8');
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+});
+
+req.on('error', (e) => {
+    console.error(`Problem with request: ${e.message}`);
+});
+
+// データを送信
+req.write(data);
+req.end();
